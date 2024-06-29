2268cd5c3034132f020abe9889ff66a31a30ad0d
Anuragkushwah21 <Anuragkushwah5100@gmail.com>
2024-06-29 15:58:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Component/Header'
+import { Route, Routes } from 'react-router-dom'
+import Login from './Component/Login'
+import Registration from './Component/Registration'
+import Dashboard from './Component/Dashboard'
+import Footer from './Component/Footer'
+import About from './Component/About'
+import Contact from './Component/Contact'
+import Profile from './Component/Profile'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+     <Routes>
+      <Route path='/' element={<Login/>}/>
+      <Route path='/registration' element={<Registration/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
